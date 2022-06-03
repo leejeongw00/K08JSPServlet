3ea4b8c4ba926b12224d89705ca04d8e2291fca1
jklw1126 <jklw1126@naver.com>
2022-06-03 18:13:04 +0900

    6월3일 수업끝!!

--- src/main/java/utils/JSFunction.java
+++ src/main/java/utils/JSFunction.java
@@ -35,4 +35,17 @@ public class JSFunction {
         catch (Exception e) {}
     }
     
+    public static String alertMsg(String msg,JspWriter out) {
+        String script="";
+          try {
+              script = ""  
+                            + "<script>"
+                            + "    alert('" + msg + "');"
+                            + "</script>";
+              out.println(script);
+          }
+          catch (Exception e) {}
+          
+          return script;
+      }
 }
